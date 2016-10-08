1ec51beb9c05dee6c5f0d604cc27160a6f349d0d
Mari Haraguchi <mariharaguchi@Mari-no-MacBook-Air.local>
2016-10-08 12:46:49 +0900

    created facebook bot

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,63 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+router.get('/webhook', function(req,res){
+	if(req.query['hub.verify_token']==='abc123'){
+		res.send(req.query['hub.challenge']);
+} else {
+	res.send('Error, wrong validation token');
+	}
+});
+
+router.post('/webhook/',function(req,res){
+	const events = req.body.entry[0].messaging;
+	for (i=0; i< events.length;i++){
+		const event = req.body.entry[0].messaging[i];
+		if(event.message && event.message.text){
+			const test= event.message.text;
+			console.log(test)
+		}
+
+	}
+res.sendStatus(200);
+});
+var request=require('request')
+const ACCESS_TOKEN=process.env.FB_ACCESS_TOKEN
+function sendTextMessage(sender,test){
+	request({
+		url:'https://graph.facebook.com/v2.6/me/messages',
+		qs:{access_token:ACCESS_TOKEN}, 
+		method:'POST',
+		json:{
+			recipient: {id:sender},
+			message:{text:text}
+		}
+	},function(error,response,body){
+		if (error){
+			concole.log('Error sending message:', error);
+		}else if (response.body.error){
+			console.log('Error:',response.body.error);
+		}
+	
+		});
+	}
+router.post('/webhook/', function (req, res) {
+	const events = req.body.entry[0].messaging;
+	for (i = 0; i < events.length; i++) {
+		const event = req.body.entry[0].messaging[i];
+		if (event.message && event.message.text) {
+			const text = event.message.text;
+			console.log(text)
+			sendTextMessage(sender,"Text received,echo:"+text);
+		}
+	}
+	res.sendStatus(200);
+});
+
+
+
+
+
+
+
+
